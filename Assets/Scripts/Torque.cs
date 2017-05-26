aa269bca58769c65a2d96ea0265287b105296d13
Sergey Blednov <sergey.blednov@pixacore.com>
2017-05-26 13:24:53 -0400

    Section completed

---
+++ Assets/Scripts/Torque.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torque : MonoBehaviour {
+
+	public Vector3 torque;
+	public float torgueTime;
+
+	Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent <Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (torgueTime >= 0f) {
+			torgueTime -= Time.deltaTime;
+			rigidBody.AddTorque (torque);
+		}
+	}
+}
